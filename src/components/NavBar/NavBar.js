9211251b65cf272ff651d6ff42150f5afbc29fe6
Sreejith P <sreejithkannan000@gmail.com>
2025-07-29 15:48:18 +0530

    updates

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,9 +8,9 @@ function NavBar() {
       <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix Logo" />
       <img
         className='avatar'
-        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-        alt="Avatar"
-        style={{ cursor: 'pointer' }}
+        src="/logo192.png"
+        alt="Profile Login"
+        style={{ cursor: 'pointer', borderRadius: '50%', background: '#181818', border: '2px solid #e50914', width: '48px', height: '48px' }}
       />
     </div>
   );
