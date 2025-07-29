f0d7d4180fadad8ecaff38080221e2afe5a1035e
Sreejith P <sreejithkannan000@gmail.com>
2025-07-29 16:07:52 +0530

    icon change

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
       <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix Logo" />
       <img
         className='avatar'
-        src="/logo192.png"
+        src="/logo1.png"
         alt="Profile Login"
         style={{ cursor: 'pointer', borderRadius: '50%', background: '#181818', border: '2px solid #e50914', width: '48px', height: '48px' }}
       />
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 export const baseURL = 'https://api.themoviedb.org/3/';
-export const API_KEY = "Your API key";
+export const API_KEY = "ca3fb9b3b783de1f95c27abcb5f51a0d";
 export const imageURL = 'https://image.tmdb.org/t/p/original';
 
